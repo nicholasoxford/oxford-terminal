5f52a510af3b0ec5e1e34933e8c07b7e0e2c2331
Nicholas Oxford <maheledaily@gmail.com>
2024-10-09 20:25:55 -0600

    metal working

--- src/objC.zig
+++ src/objC.zig
@@ -2,6 +2,7 @@ const objc = @import("zig-objc");
 const std = @import("std");
 pub fn macosVersionAtLeast(major: i64, minor: i64, patch: i64) !bool {
     // Get the objc class from the runtime
+    const metalInfo = objc.getClass("metal").?;
     const NSProcessInfo = objc.getClass("NSProcessInfo").?;
 
     std.log.debug("major: {d}, minor: {d}, patch: {d}", .{ major, minor, patch });
@@ -9,7 +10,8 @@ pub fn macosVersionAtLeast(major: i64, minor: i64, patch: i64) !bool {
     // Call a class method with no arguments that returns another objc object.
     const info = NSProcessInfo.msgSend(objc.Object, "processInfo", .{});
 
-    std.log.debug("info: {any}", .{info});
+    const device = metalInfo.msgSend(objc.Object, "MTLCreateSystemDefaultDevice", .{});
+    std.log.debug("device: {any}", .{device});
     // return true;
     // Call an instance method that returns a boolean and takes a single
     // argument.
